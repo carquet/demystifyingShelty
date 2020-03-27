6fcd61e40da685d8f824bca4797298036158ece7
L.Carquet <laetitiacarquet@gmail.com>
2020-03-27 09:27:56 +0100

    lcreates and links second source

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,9 @@ class ApplicationController < ActionController::Base
 	def show_dog
 		
 		dog = Dog.find(params['id'])
+		comments = connection.execute('SELECT * FROM comments WHERE comments.dog_id = ?', params['id'])
 
-		render 'application/show_dog', locals: {dog: dog}
+		render 'application/show_dog', locals: {dog: dog, comments: comments}
 		
 	end
 
@@ -57,7 +58,11 @@ class ApplicationController < ActionController::Base
 		
 	end
 
-	
+	def connection
+		connection = SQLite3::Database.new 'db/development.sqlite3'
+		connection.results_as_hash = true
+		connection
+	end
 
 
 	
---
+++ db/comments.sql
@@ -0,0 +1,5 @@
+CREATE TABLE comments(
+	"id" INTEGER PRIMARY KEY NOT NULL,
+	"body" varchar,
+	"potowner" varchar,
+	"dog_id" integer  );
